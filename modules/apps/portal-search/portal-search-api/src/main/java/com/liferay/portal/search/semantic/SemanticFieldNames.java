a7a58794bc3c44d356e49376dc44562e8869e3e6
Rodrigo Guedes de Souza <rodrigo.guedes@liferay.com>
2026-05-26 15:19:51 -0300

    LPD-90490 LPD-90849 portal-search-api Add SemanticFieldNames public API for strategy-aware semantic field naming https://liferay.atlassian.net/browse/LPD-90490

---
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/semantic/SemanticFieldNames.java
@@ -0,0 +1,22 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.semantic;
+
+import java.util.Locale;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Rodrigo Guedes
+ */
+@ProviderType
+public interface SemanticFieldNames {
+
+	public String fieldName(
+		Locale locale, SemanticProviderType semanticProviderType,
+		String assetType, int dimensions);
+
+}
